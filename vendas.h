b57826f7352e3078370fac2219ba74d792951cdb
PhantomWorld <heider_yagami@hotmail.com>
2022-03-16 11:07:31 -0300

    Vendas commit

---
+++ vendas.h
@@ -0,0 +1,28 @@
+#ifndef TPFINAL_VENDAS_H
+#define TPFINAL_VENDAS_H
+
+
+class Vendas {
+private:
+ 
+  double _faturamento;
+
+  int _porcentagemvenda;
+
+  int _quantidadevendida;
+
+  int _vendadia;
+
+  int _vendames;
+
+  int _vendaano;
+
+
+  public:
+
+  maisvendidos(std:string);
+
+
+};
+
+#endif
