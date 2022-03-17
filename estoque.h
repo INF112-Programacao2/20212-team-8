b582161c1155ed094f81f6a5a28cccbd2fb54bd0
PhantomWorld <heider_yagami@hotmail.com>
2022-03-16 22:38:15 -0300

    estoque commit

---
+++ estoque.h
@@ -0,0 +1,19 @@
+#ifndef TPFINAL_Estoque_H
+#define TPFINAL_Estoque_H
+
+
+class Estoque{
+ private:
+
+  int _estoque;
+
+
+ public:
+     
+     Estoque();
+
+     void fluxoproduto();
+
+};
+
+#endif
