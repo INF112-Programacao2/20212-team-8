bf2500d5d9a3cf181da857b62ccfa97b649eb955
PhantomWorld <heider_yagami@hotmail.com>
2022-03-16 23:18:36 -0300

    jogos commit

---
+++ jogos.h
@@ -0,0 +1,24 @@
+#ifndef TPFINAL_Jogos_H
+#define TPFINAL_Jogos_H
+#include <string>
+
+class Jogos{
+  private:
+
+  
+  std::string _categoria;
+  std::string _plataforma;
+
+  public:
+
+
+  void filtroaz();
+  void listanomecapa();
+  void listaplataformas();
+  void maisvendidos();
+  void pesquisar();
+  void filtrolancamento();
+
+};
+
+#endif
