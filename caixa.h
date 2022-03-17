187eb5930efbbe72c76c5ae6099bb651a24dc73e
Michael5Horizons <michael.basilio@ufv.br>
2022-03-16 22:23:39 -0300

    commit2

---
+++ caixa.h
@@ -0,0 +1,18 @@
+#ifndef TPFINAL_CAIXA_H
+#define TPFINAL_CAIXA_H
+
+class Caixa {
+private:
+    
+    double _saldo;
+    double _fluxocaixa;
+
+public:
+    
+    Caixa();
+    void registro();
+
+
+};
+
+#endif
