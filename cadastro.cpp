b70f0da10158180f5935518d42e061c510a9a730
Michael5Horizons <michael.basilio@ufv.br>
2022-03-21 11:32:35 -0300

    commit cadastro

--- cadastro.cpp
+++ cadastro.cpp
@@ -1,34 +1,45 @@
 #include "cadastro.h"
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <fstream>
 
 Cadastro::Cadastro(){
-    _nome = NULL;
-    _genero = NULL;
-    _datanascimento = NULL;
-    _cpf = NULL;
-    _telefone = NULL;
-    _email = NULL;
-    _endereco = NULL;
-    _senha = NULL;
-    _nomeproduto = NULL;
-    _codigoproduto = NULL;
-    _precoproduto = NULL;
-    _quantidadeproduto = NULL;
-    _descricaoproduto = NULL;
-}
-
-void Cadastro::cadastrarconta(){
-    std::cin >> _nome;
-    std::cin >> _genero;
-    std::cin >> _datanascimento;
-    std::cin >> _cpf;
-    std::cin >> _telefone;
-    std::cin >> _email;
-    std::cin >> _endereco;
-    std::cin >> _senha;
+    std::getline(std::cin, _nome); 
+	std::getline(std::cin,_genero);
+	std::getline(std::cin, _datanascimento);
+	std::getline(std::cin, _cpf);
+	std::getline(std::cin, _telefone); 
+	std::getline(std::cin, _email); 
+	std::getline(std::cin, _endereco);
+	std::getline(std::cin, _senha);
    
+    std::string arquivot1("conta.txt");
+    std::ifstream acc_entrada;
+    std::ofstream acc_saida;
+
+    acc_entrada.open(arquivot1,std::ios_base::app);
+	
+	acc_saida.open(arquivot1,std::ios::out);
+	
+	acc_saida << "Nome: ";
+    acc_saida << _nome << std::endl;
+	acc_saida << "Genero: ";
+    acc_saida << _genero << std::endl;
+    acc_saida << "Data de nascimento: ";
+	acc_saida << _datanascimento << std::endl;
+    acc_saida << "CPF: ";
+	acc_saida << _cpf << std::endl;
+    acc_saida << "Telefone: ";
+	acc_saida << _telefone << std::endl;
+    acc_saida << "Email: ";
+	acc_saida << _email << std::endl;
+    acc_saida << "Endereco: ";
+	acc_saida << _endereco << std::endl;
+    acc_saida << "Senha: ";
+	acc_saida << _senha << std::endl;
+    
+    acc_saida.close();
 }
 
 void Cadastro::cadastrarproduto(){
