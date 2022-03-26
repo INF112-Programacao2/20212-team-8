2ebb2709e62e9b901e3733be7cb16d102f2d88a0
Michael5Horizons <michael.basilio@ufv.br>
2022-03-25 23:06:52 -0300

    commit cadastro

--- cadastro.cpp
+++ cadastro.cpp
@@ -5,7 +5,16 @@
 #include <fstream>
 
 Cadastro::Cadastro(){
-    std::getline(std::cin, _nome); 
+   std::cout << "Ja possui conta? Se sim, digite 'S' para ir para o login." << std::endl;
+   std::cout << "Se nao, digite 'N' para criar uma conta" << std::endl;
+   char option;
+   
+   std::cin >> option;
+   if(option == 'S' || option == 's'){
+	  
+   }else if(option == 'N' || option == 'n'){
+	std::cin.ignore();
+	std::getline(std::cin, _nome); 
 	std::getline(std::cin,_genero);
 	std::getline(std::cin, _datanascimento);
 	std::getline(std::cin, _cpf);
@@ -22,24 +31,25 @@ Cadastro::Cadastro(){
 	
 	acc_saida.open(arquivot1,std::ios::out);
 	
-	acc_saida << "Nome: ";
+	acc_saida << "#";
     acc_saida << _nome << std::endl;
-	acc_saida << "Genero: ";
+	acc_saida << "#";
     acc_saida << _genero << std::endl;
-    acc_saida << "Data de nascimento: ";
+    acc_saida << "#";
 	acc_saida << _datanascimento << std::endl;
-    acc_saida << "CPF: ";
+    acc_saida << "#";
 	acc_saida << _cpf << std::endl;
-    acc_saida << "Telefone: ";
+    acc_saida << "#";
 	acc_saida << _telefone << std::endl;
-    acc_saida << "Email: ";
+    acc_saida << "#";
 	acc_saida << _email << std::endl;
-    acc_saida << "Endereco: ";
+    acc_saida << "#";
 	acc_saida << _endereco << std::endl;
-    acc_saida << "Senha: ";
+    acc_saida << "#";
 	acc_saida << _senha << std::endl;
     
     acc_saida.close();
+   }
 }
 
 void Cadastro::cadastrarproduto(){
@@ -57,7 +67,11 @@ void Cadastro::cadastrarproduto(){
 }
 
 void Cadastro::temcadastro(){
-    std::cout << "Para efetuar login na nossa plataforma, digite seu email e apos confirmar, digite sua senha: ";
+	char captura[200];
+	char teste[200];
+	char *t;
+	bool acc_vrfy = true;
+	std::cout << "Para efetuar login na nossa plataforma, digite seu email e apos confirmar, digite sua senha: ";
     
     std::string email;
     std::cin >> email;
@@ -65,9 +79,48 @@ void Cadastro::temcadastro(){
     std::string senha;
     std::cin >> senha;
 
-    ifstream acc_check;
-    
+    std::ifstream acc_check;
+    acc_check.open("conta.txt",std::ios::in);
+	
+		
+	for(int i = 0; i < 8; i++){	
+		acc_check.getline(captura,200);
 
-
-    
+		if(i == 5){
+			t = strtok(captura,"#");		//Separação da string em tokens
+					
+			while(t != NULL){
+				strcpy(teste, t);		//Captura do nome
+				t = strtok(NULL,"#");
+			}
+			
+			if(email != teste){
+				acc_vrfy = false;
+			}
+		}if(i == 7){
+			t = strtok(captura,"#");		//Separação da string em tokens
+					
+			while(t != NULL){
+				strcpy(teste, t);		//Captura do nome
+				t = strtok(NULL,"#");
+			}
+	
+			if(senha != teste && acc_vrfy != false){
+				acc_vrfy = false;
+			}
+			
+		}if(i == 7 && acc_vrfy == true){
+			std::cout << "Login realizado com sucesso!" << std::endl;
+			std::cout << "Transferindo para a pagina inicial..." << std::endl;
+		}else if(i == 7 && acc_vrfy != true){
+			std::cout << "Email ou senha incorretos! Tente novamente." << std::endl;
+			acc_check.close();
+			std::cin >> email;
+			std::cin >> senha;
+			i = -1;
+			acc_vrfy = true;
+			acc_check.open("conta.txt",std::ios::in);
+		}
+	}
+	
 }
