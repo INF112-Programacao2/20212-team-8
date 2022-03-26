3af17403e9edd3b4070b9ae0e2482459ce4c0d70
PhantomWorld <heider_yagami@hotmail.com>
2022-03-26 00:48:19 -0300

    compra commit

--- compra.cpp
+++ compra.cpp
@@ -2,6 +2,7 @@
 #include "compra.h"
 #include "conta.h"
 #include <ctime>
+#include <fstream>
 
 Compra::Compra(){
 
@@ -19,14 +20,33 @@ _numeropedido = 1+rand()%100;
 std::cout << "Digite a data: ";
 std::cin >> _data;
 
-_cliente= _nome;
+_cliente = _nome;
 
 }
 
 
-void Compra::emitirboleto(){
+void Compra::emitirrecibo(){
 
 
+    std::string arquivor("recibo.txt");
+    std::ifstream acc_entrada;
+    std::ofstream acc_saida;
+
+    acc_entrada.open(arquivor,std::ios_base::app);
+	acc_saida.open(arquivor,std::ios::out);
+     
+
+     acc_saida << "Olá ";
+     acc_saida << _nome << std::endl;
+     acc_saida << " Obrigado por comprar na nossa loja." << std::endl;
+     acc_saida << "Nome do Produto: " << _nomeproduto << std::endl;
+     acc_saida << "Preco do produto: " << _precoproduto << std::endl;
+
+     acc_saida.close();
 
 }
+
+
+
+
 
