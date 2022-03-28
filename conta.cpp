78978fa88478f54873805c4ccb8e91b9a34902cc
PhantomWorld <heider_yagami@hotmail.com>
2022-03-27 21:42:13 -0300

    commit

--- conta.cpp
+++ conta.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 #include "conta.h"
+#include <fstream>
 
 Conta::Conta(){
 
 
 }
+
+void contapadrao(){
+
+
+
+
+
+}
+
+void contapremium(){
+
+
+    
+}
+
+void pedido(){
+
+
+    std::string arquivop("pedido.txt");
+    std::ifstream acc_entrada;
+    std::ofstream acc_saida;
+
+    acc_entrada.open(arquivop,std::ios_base::app);
+	
+	acc_saida.open(arquivop,std::ios::out);
+
+}
+
 
