8092d4525d6db7dbf347a21bb94ac114c2dd9cfc
PhantomWorld <heider_yagami@hotmail.com>
2022-03-27 22:40:18 -0300

    commit

--- estoque.cpp
+++ estoque.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 #include "estoque.h"
+
+
+Estoque::Estoque(){
+
+   
+   int _estoque = 0;
+
+}
+
+
+
+void Estoque::fluxoproduto(){
+
+
+    _fluxoproduto = estoque - quantidade;
+
+}
