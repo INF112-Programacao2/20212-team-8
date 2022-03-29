868f64cee420c6b34adde21b4cb0ad17d5a4685f
Michael5Horizons <michael.basilio@ufv.br>
2022-03-29 14:46:39 -0300

    commit Caixa

--- caixa.cpp
+++ caixa.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 #include "caixa.h"
 
 
@@ -34,9 +35,9 @@ void Caixa::registro(){
       if(h == 0){
          nome = dados;
       }if(h == 1){
-         quantidade = atoi(dados);
-      }if(h == 3){
-         valor = atoi(dados);
+         quantidade = atof(dados);
+      }if(h == 2){
+         valor = atof(dados);
       }
 
       h++;
@@ -50,42 +51,54 @@ void Caixa::registro(){
       acc_reg.open("registro.txt",std::ios_base::app);
       acc_regout.open("registro.txt",std::ios::out);
 
-      _saldo += quantidade*valor;
+      _saldo += valor;
 
       acc_regout << "Saldo em caixa: R$" << _saldo << std::endl;
       acc_regout << "::Registro de vendas::" << std::endl;
-      acc_regout << quantidade << "x " << nome << " (R$ " << valor << ")" << std::endl;
+      acc_regout << quantidade << "x " << nome << " (R$ " << std::fixed << std::setprecision(2) << valor << ")" << std::endl;
       acc_regout << "+" << std::endl;
 
       acc_regout.close();
 
    }else{
       char captura[100][100];
+	  char captura2[100];
       int i = 0;
-		
+	  char *t;
+	  
 		while(true){
 			if(acc_reg.eof()){
 				break;
 			}
 			acc_reg.getline(captura[i],100);
          if(i == 0){
-            _saldo = atoi(captura[i]);
+			t = strtok(captura[i],"Saldo em caixa: R$");		//Separação da string em tokens
+			
+			while(t != NULL){
+				strcpy(captura2, t);		//Captura do nome
+				t = strtok(NULL,"Saldo em caixa: R$");
+			}
+            _saldo = atof(captura2);
          }
 			i++;
 		}
-
-      _saldo = quantidade*valor;
-
-      acc_regout.open("registro.txt",std::ios::out);
+	  
+		  acc_reg.close();
+		  
+		  _saldo += valor;
+			
+		  acc_regout.open("registro.txt",std::ios::out);
 		
 		for(int j = 0; j < i; j++){
 			if(j == i-1){
 				acc_regout << captura[j];
-			}else{
+			}else if(j == 0){
+				acc_regout << "Saldo em caixa: R$" << std::fixed << std::setprecision(2) << _saldo << std::endl;
+			}else if(j != 0 & j != i-1){
 				acc_regout << captura[j] << std::endl;
 			}
 		}
-
+	
       acc_regout << quantidade << "x " << nome << " (R$ " << valor << ")" << std::endl;
       acc_regout << "+" << std::endl;
       
