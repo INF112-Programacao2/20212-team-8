cc8194a7d7f1e62deca9ed7a609ab1324a824228
Michael5Horizons <michael.basilio@ufv.br>
2022-03-29 22:17:43 -0300

    commit Desconto

--- desconto.cpp
+++ desconto.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <cstring>
 
-Desconto::Desconto(std::string verifica){
+Desconto::Desconto(std::string verifica){	//String verifica se e conta normal ou premium
     int n = 0;
     char comentario1[100];
     double valor;
@@ -18,7 +18,7 @@ Desconto::Desconto(std::string verifica){
         
 		enter_descont.getline(comentario1,100);
 
-        if(n == 2){
+        if(n == 2){	//Pega o valor da compra
             valor = atof(comentario1);
             break;
         }
@@ -26,28 +26,28 @@ Desconto::Desconto(std::string verifica){
         n++;
     }
 
-    if(valor > 100 && verifica == "normal"){
+    if(valor > 100 && verifica == "normal"){	//Compras acima de 100 reais e sendo conta normal
         std::cout << "Desconto de 20% " << "disponivel!" << std::endl;
         _desconto = 0.20;
-		_verifica = true;
+		_verifica = true;	//confirma desconto
     }
 
-    else if(verifica == "premium"){
+    else if(verifica == "premium"){	//Conta premium
         std::cout << "E assim que a gente gosta, e a PREMIUM trazendo economias para voce!" << std::endl;
         std::cout << "Desconto de 30% " << "preparado!" << std::endl;
         _desconto = 0.30;
 		_verifica = true;
     }
 
-    else if(valor < 100 && verifica == "normal"){
+    else if(valor < 100 && verifica == "normal"){	//Compras abaixo de 100 reais e conta normal
         std::cout << "Sinto muito! Mas o senhor(a) nao possui descontos disponiveis." << std::endl;
-		_verifica = false;
+		_verifica = false;	//desconto nao existente
     }
 
 }
 
 void Desconto::descontar(){
-	if(_verifica == true){
+	if(_verifica == true){	//Se confirmou desconto
 	
 		int n = 0;
 		char comentario1[100][100];
@@ -66,7 +66,7 @@ void Desconto::descontar(){
 			
 			enter_descont.getline(comentario1[n],100);
 
-			if(n == 2){
+			if(n == 2){	//Pega valor da compra
 
 				valor = atof(comentario1[n]);
 			}
@@ -79,25 +79,25 @@ void Desconto::descontar(){
 
 		valor -= (valor*_desconto);                         // efetuando o desconto
 		
-		out_descont.open("compra.txt",std::ios::out);
+		out_descont.open("compra.txt",std::ios::out);	//Inicializa saida do compra.txt
 		
 		for(int i = 0; i < n; i++){
-			if(i == n-2){
+			if(i == n-2){	//Ultima linha
 				break;
 			}
 
-			out_descont << comentario1[i] << std::endl;
+			out_descont << comentario1[i] << std::endl;	//Saida de linha anteriores do compra.txt
 		
 		}
 		
-		out_descont << valor << std::endl;
+		out_descont << valor << std::endl;	//Saida com o valor atualizado da compra com o desconto
 		
 		out_descont.close();
 		
 	}
 	
 	
-	else{
+	else{	//Se nao foi encontrado desconto, faz nada
 		
 	}
 
