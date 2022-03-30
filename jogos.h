8b34b8cace69f0e43b75429a2b7f2ab1114b44f9
Michael5Horizons <michael.basilio@ufv.br>
2022-03-29 22:30:23 -0300

    commit Jogos

--- jogos.h
+++ jogos.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef TPFINAL_Jogos_H
 #define TPFINAL_Jogos_H
 #include <string>
@@ -12,18 +11,3 @@ class Jogos{
 };
 
 #endif
-=======
-#ifndef TPFINAL_Jogos_H
-#define TPFINAL_Jogos_H
-#include <string>
-
-
-class Jogos{
-  public:
-
-  virtual void exibirjogos() = 0;
-  
-};
-
-#endif
->>>>>>> ed5deb0bfef70ee5dc9a56ad610441e26a535944
